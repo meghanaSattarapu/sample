d56386a540ac6b8209b8a2c91a0e000135f10c10
meghanaSattarapu <163449773+meghanaSattarapu@users.noreply.github.com>
2025-07-29 11:01:56 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -6,6 +6,6 @@ public class sample {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        System.out.println("Hello");
     }
 }
